f78c640c6d83188827b30502bb62443552bfb7c9
Gabriel <gborges9@outlook.com>
2022-12-14 15:30:57 -0300

    Refactor about me page

--- lib/src/features/about_me/ui/about_me_page.dart
+++ lib/src/features/about_me/ui/about_me_page.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_portfolio/src/features/about_me/ui/widgets/about_section.dart';
-import 'package:flutter_portfolio/src/features/app/theme/color.dart';
-import 'package:flutter_portfolio/src/features/app/theme/margins.dart';
-import 'package:flutter_portfolio/src/features/app/ui/widgets/responsive_section.dart';
-import 'package:gbx_core/gbx_core.dart';
 
 class AboutMePage extends StatelessWidget {
   const AboutMePage({super.key});
