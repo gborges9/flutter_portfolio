9b2ae790d35c0b6ef8c8c0d19695633b450f9a58
Gabriel <gborges9@outlook.com>
2022-12-14 15:56:31 -0300

    made about section responsive

--- lib/src/features/about_me/ui/widgets/about_section.dart
+++ lib/src/features/about_me/ui/widgets/about_section.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_breakpoints/adaptive_breakpoints.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_portfolio/src/features/about_me/ui/widgets/download_cv_button.dart';
 import 'package:flutter_portfolio/src/features/app/theme/borders.dart';
@@ -16,77 +17,105 @@ class AboutSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final windowType = getWindowType(context);
+    bool isSmallScreen = windowType < AdaptiveWindowType.medium;
     return ResponsiveSection(
-        backgroundColor: AppColors.darkerBackground,
-        child: Padding(
-          padding: EdgeInsets.all(AppMargins.cardPadding),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      Strings.of(context).name,
-                      style:
-                          Theme.of(context).textTheme.displayMedium!.copyWith(
-                                color: AppColors.black,
-                                fontWeight: FontWeight.w900,
-                              ),
-                    ),
-                    AppMargins.halfPadding.ph,
-                    Text(
-                      Strings.of(context).jobTitle,
-                      style:
-                          Theme.of(context).textTheme.headlineSmall!.copyWith(
-                                color: AppColors.darkGrey,
-                              ),
-                    ),
-                    AppMargins.quadPadding.ph,
-                    Text(
-                      Strings.of(context).about,
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                            color: AppColors.black,
-                          ),
-                    ),
-                    AppMargins.quadPadding.ph,
-                    Center(
-                      child: Wrap(
-                        alignment: WrapAlignment.center,
-                        children: [
-                          const DownloadCvButton(),
-                          AppMargins.doublePadding.pw,
-                          TextButton.icon(
-                            onPressed: () {},
-                            icon: const HeroIcon(HeroIcons.envelope),
-                            label: Text(Strings.of(context).contactButton),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+      backgroundColor: AppColors.darkerBackground,
+      child: Padding(
+        padding: EdgeInsets.all(AppMargins.cardPadding),
+        child: Column(
+          children: [
+            if (isSmallScreen) profileImage(context),
+            if (isSmallScreen) AppMargins.doublePadding.ph,
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: introItems(context, isSmallScreen),
                 ),
-              ),
-              AppMargins.appBorder.pw,
-              Card(
-                elevation: AppElevation.elevation1,
-                shape: RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(AppBorders.cardBorder / 2)),
-                child: ClipRRect(
-                  borderRadius:
-                      BorderRadius.circular(AppBorders.cardBorder / 2),
-                  child: Image.asset(
-                    "assets/images/image_1.jpeg",
-                    fit: BoxFit.cover,
-                    width: 320,
+                if (!isSmallScreen) AppMargins.appBorder.pw,
+                if (!isSmallScreen)
+                  SizedBox(
                     height: 320,
-                  ),
-                ),
+                    width: 320,
+                    child: trailingImage(context),
+                  )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget introItems(BuildContext context, bool center) {
+    final textAlign = center ? TextAlign.center : TextAlign.start;
+    return Column(
+      crossAxisAlignment:
+          center ? CrossAxisAlignment.center : CrossAxisAlignment.start,
+      children: [
+        Text(
+          Strings.of(context).name,
+          style: Theme.of(context).textTheme.displayMedium!.copyWith(
+                color: AppColors.black,
+                fontWeight: FontWeight.w900,
+              ),
+          textAlign: textAlign,
+        ),
+        AppMargins.halfPadding.ph,
+        Text(
+          Strings.of(context).jobTitle,
+          style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                color: AppColors.darkGrey,
+              ),
+          textAlign: textAlign,
+        ),
+        AppMargins.quadPadding.ph,
+        Text(
+          Strings.of(context).about,
+          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                color: AppColors.black,
+              ),
+          textAlign: textAlign,
+        ),
+        AppMargins.quadPadding.ph,
+        Center(
+          child: Wrap(
+            alignment: WrapAlignment.center,
+            children: [
+              const DownloadCvButton(),
+              AppMargins.doublePadding.pw,
+              TextButton.icon(
+                onPressed: () {},
+                icon: const HeroIcon(HeroIcons.envelope),
+                label: Text(Strings.of(context).contactButton),
               ),
             ],
           ),
-        ));
+        ),
+      ],
+    );
+  }
+
+  Widget trailingImage(BuildContext context) {
+    return Card(
+      elevation: AppElevation.elevation1,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppBorders.cardBorder / 2)),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(AppBorders.cardBorder / 2),
+        child: Image.asset(
+          "assets/images/image_1.jpeg",
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget profileImage(BuildContext context) {
+    return const CircleAvatar(
+      foregroundImage: AssetImage("assets/images/profile.jpeg"),
+      radius: 88,
+    );
   }
 }
